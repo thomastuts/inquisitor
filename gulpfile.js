baca9d95741ba06bbdedebf4ae1cdc0468544c46
Thomas Tuts <thomas@darwinanalytics.com>
2014-07-29 18:04:28 +0200

    Add Karma test task to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,27 @@ var gulp = require('gulp');
 var annotate = require('gulp-ng-annotate');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var karma = require('karma').server;
+
+var karmaConfig = require('./karma.conf.js');
 
 var paths = {
   inquirer: 'src/inquirer.js',
   dist: 'dist/'
 };
 
+gulp.task('test', function (done) {
+  karma.start(_.assign({}, karmaConfig, {singleRun: true}), done);
+});
+
 gulp.task('inquirer-dist', function () {
-  gulp.src(paths.inquirer)
+  return gulp.src(paths.inquirer)
     .pipe(annotate())
     .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('inquirer-min-dist', function () {
-  gulp.src(paths.inquirer)
+  return gulp.src(paths.inquirer)
     .pipe(annotate())
     .pipe(uglify())
     .pipe(rename({
